7840f807df052ac9b69bb99ec22b1a59aacd9a30
Ramsey :) <113808661+Ramsey-Austria@users.noreply.github.com>
2023-01-07 00:04:33 +0000

    Delete Femboyware directory

--- Femboyware/Femboyware/ext/SDK/CMath/CMath.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <Includes.h>
-
-namespace CMath
-{
-	Vectors::Vec2 WorldToScreen( Vectors::Vec3 ScreenPos );
-	Vectors::Vec3 GetBone( DWORD64 ped , int32_t bone );
-	Vectors::Vec2 GetBoneWorldToScreen( DWORD64 ped , int32_t bone );
-}
--- Femboyware/Femboyware/src/Features/Aimbot/Aimbot.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <Includes.h>
-
-namespace Aimbot
-{
-	void Aimbot( );
-}
